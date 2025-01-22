1a03eb435a1a3309d8ebfab41ed68073651e5c92
getdn <getdna99@gmail.com>
2025-01-22 08:48:07 -0800

    Update UI Data

--- UI Data/ThemeResources.cs
+++ UI Data/ThemeResources.cs
@@ -3,7 +3,7 @@ using static Get.Data.Properties.AutoTyper;
 namespace Get.UI.Data;
 public partial class ThemeResources
 {
-    public static IReadOnlyProperty<T> Create<T>(string resourcesName, FrameworkElement ele)
+    public static IReadOnlyProperty<T> Get<T>(string resourcesName, FrameworkElement ele)
     {
         if (!ele.Resources.TryGetValue(resourcesName, out var val)) val = Application.Current.Resources[resourcesName];
         var prop = new Property<T>((T)val);
@@ -11,6 +11,15 @@ public partial class ThemeResources
         {
             if (!ele.Resources.TryGetValue(resourcesName, out var val)) val = Application.Current.Resources[resourcesName];
             prop.Value = (T)val;
+        };
+        return AutoReadOnly<T>(prop);
+    }
+    public static IReadOnlyProperty<T> Create<T>(FrameworkElement ele, T darkResource, T lightResource)
+    {
+        var prop = new Property<T>(ele.ActualTheme is ElementTheme.Dark ? darkResource : lightResource);
+        ele.ActualThemeChanged += delegate
+        {
+            prop.Value = ele.ActualTheme is ElementTheme.Dark ? darkResource : lightResource;
         };
         return AutoReadOnly<T>(prop);
     }
